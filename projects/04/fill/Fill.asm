6593a06613c9ab39b570d83e59afa98060241115
panpan-wu <wupanpan8@163.com>
2020-12-17 11:31:36 +0800

    Project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,120 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// n = 512 * 256 / 16 = 8192
+    @8192
+    D=A
+    @n
+    M=D
+(LOOP)
+    @KBD
+    D=M
+    @WHITE
+    D;JEQ
+
+  // BLACK START
+    @i
+    M=0
+(LOOP_BLACK)
+    @i
+    D=M
+    @n
+    D=D-M
+    @LOOP
+    D;JEQ
+
+    @SCREEN
+    D=A
+    @i
+    A=D+M
+    M=-1
+    @i
+    M=M+1
+    @LOOP_BLACK
+    0;JMP
+    // BLACK END
+
+    @LOOP
+    0;JMP 
+(WHITE)
+    // WHITE START
+    @i
+    M=0
+(LOOP_WHITE)
+    @i
+    D=M
+    @n
+    D=D-M
+    @LOOP
+    D;JEQ
+
+    @SCREEN
+    D=A
+    @i
+    A=D+M
+    M=0
+    @i
+    M=M+1
+    @LOOP_WHITE
+    0;JMP
+    // WHITE END
+
+    @LOOP
+    0;JMP
+
+//   @SCREEN
+//   D=A
+//   @cursor
+//   M=D
+// 
+//   @8192 // 256 x 32 (rows x 16-bit words)
+//   D=A
+//   @SCREEN
+//   D=D+A
+//   @size
+//   M=D
+// 
+// (LOOP)
+//   @KBD
+//   D=M
+//   @WHITE
+//   D;JEQ
+//   @BLACK
+//   0;JMP
+// 
+// (WHITE)
+//   @color
+//   M=0
+//   @FILL
+//   0;JMP
+// 
+// (BLACK)
+//   @color
+//   M=-1
+//   @FILL
+//   0;JMP
+// 
+// (FILL)
+//   @color
+//   D=M
+//   @cursor
+//   A=M
+//   M=D
+//   @cursor
+//   M=M+1
+//   D=M
+//   @size
+//   D=D-M
+//   @RESETCURSOR
+//   D;JGT
+//   @LOOP
+//   0;JMP
+// 
+// (RESETCURSOR)
+//   @SCREEN
+//   D=A
+//   @cursor
+//   M=D
+//   @LOOP
+//   0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,65 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// n = -R1 if R1 < 0 else R1
+    @R1
+    D=M
+    @R1_LESS_THAN_ZERO_0
+    D;JLT
+    @n
+    M=D
+    @END_N
+    0;JMP
+(R1_LESS_THAN_ZERO_0)
+    @n
+    M=-D
+(END_N)
+
+// mul = 0
+    @mul
+    M=0
+// i = 0
+    @i
+    M=0
+
+(LOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @END_LOOP
+    D;JEQ
+
+    // mul = mul + R0
+    @mul
+    D=M
+    @R0
+    D=D+M
+    @mul
+    M=D
+    // i = i + 1
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END_LOOP)
+    @R1
+    D=M
+    @R1_LESS_THAN_ZERO_1
+    D;JLT
+    @mul
+    D=M
+    @R2
+    M=D
+    @END
+    0;JMP
+(R1_LESS_THAN_ZERO_1)
+    @mul
+    D=M
+    @R2
+    M=-D
+
+(END)
+    @END
+    0;JMP
